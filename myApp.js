024b0bd466f946f542c75c1f8615de0f79e5ec7e
Mladenka Orlić <mladenka.orlic@gmail.com>
2024-12-01 23:24:28 +0000

    try to add body parser mware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+let bodyParser=this.require("body-parser");
 
 //console.log("Hello World");
 
@@ -13,6 +14,8 @@ app.use((req,res,next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/public",express.static(__dirname+"/public"));
 
